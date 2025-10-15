f777615a34bf122e134db94815db2a2ab83c8743
Belal Sejouk <billoosijok@gmail.com>
2025-10-16 01:27:52 +0200

    Build and publish site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 import TopBar from "@/components/TopBar";
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
-import Stats from "@/components/Stats";
 import Services from "@/components/Services";
-import Experience from "@/components/Experience";
-import Partnership from "@/components/Partnership";
-import SafeMoving from "@/components/SafeMoving";
 import Testimonials from "@/components/Testimonials";
-import Experts from "@/components/Experts";
-import News from "@/components/News";
 import ContactInfo from "@/components/ContactInfo";
 import Footer from "@/components/Footer";
 
